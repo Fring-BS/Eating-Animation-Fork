64a4bc305c16a5304df98a6c2b978ca1d6964f13
Fring <saadimran4008@gmail.com>
2026-07-31 11:07:43 +0500

    Fix import statement for Identifier class

--- src/main/java/ru/tpsd/eatinganimationmod/EatingAnimationClientMod.java
+++ src/main/java/ru/tpsd/eatinganimationmod/EatingAnimationClientMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.resources.Identifier;  // ← NOT ResourceLocation!
+import net.minecraft.resources.Identifier;
 
 public class EatingAnimationClientMod implements ClientModInitializer {
 
